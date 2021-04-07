742c1f85dd9b7f73bbae538439651350cac0812f
Kevin Boss <kevin.boss@outlook.com>
2021-04-07 17:14:50 +0200

    Creating a Model and ViewModel

--- Todo/Todo/ViewModels/MainWindowViewModel.cs
+++ Todo/Todo/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Todo.Services;
 
 namespace Todo.ViewModels
 {
-	public class MainWindowViewModel : ViewModelBase
-	{
-	}
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel(Database db)
+        {
+            List = new TodoListViewModel(db.GetItems());
+        }
+
+        public TodoListViewModel List { get; }
+    }
 }
